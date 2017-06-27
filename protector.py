ac5d50a98248e341aca613f158ebe7982c093f92
Luan <Luan>
2017-06-27 11:52:09 -0300

    Atualização

--- protector.py
+++ protector.py
@@ -16,8 +16,8 @@ def dropdown():
 	driver.find_element_by_xpath("//*[@id='header']/div[2]/ul[1]/li[2]/a").click()
 	WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='header']/div[2]/ul[1]/li[2]/ul/li[1]")))
 
-#site = 'http://qa2.strider.io/user/#/signin' 
-site = 'http://painel.strider.ag/user/#/signin' 
+site = 'http://qa2.strider.io/user/#/signin' 
+#site = 'http://painel.strider.ag/user/#/signin' 
 driver = webdriver.Chrome("C:/dev/chromedriver.exe")
 
 delay = 50
@@ -25,16 +25,15 @@ driver.get(site) #URL do site alvo
 
 
 # Inicio Login
-
 driver.find_element_by_xpath("//*[@id='content']/div/div[3]/div/div/form/fieldset/div[5]/div/input").send_keys("luan")
 driver.find_element_by_xpath("//*[@id='content']/div/div[3]/div/div/form/fieldset/div[8]/div/input").send_keys("luan1")
 driver.find_element_by_xpath("//*[@id='content']/div/div[3]/div/div/form/fieldset/div[11]/a").click()
 print('Teste Login: Done')
 # Final Login
 
-#Selecionar a fazenda 4 (Belo Horizonte)
-WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='content']/div/div[4]/div/div[3]/section/div[1]/div[2]/span")))
-driver.find_element_by_xpath("//*[@id='content']/div/div[4]/div/div[4]/section/div[1]/div[2]/span").click()
+#Selecionar a fazenda 1
+WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='content']/div/div[4]/div/div[1]/section/div[1]/div[1]")))
+driver.find_element_by_xpath("//*[@id='content']/div/div[4]/div/div[1]/section/div[1]/div[1]").click()
 
 #Espera a linha do tempo carregar
 WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='region-tree-nodes']/li/ol/li[1]/div")))
@@ -138,5 +137,4 @@ dropdown()
 driver.find_element_by_xpath("//*[@id='header']/div[2]/ul[1]/li[2]/ul/li[15]").click() #Menu Customização
 driver.find_element_by_xpath("//*[@id='header']/div[2]/ul[1]/li[2]/ul/li[15]/ul/li[2]").click() #Problemas
 WebDriverWait(driver, delay).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='content']/div/div[1]/div/div/div"))) #Botão Salvar
-
 
