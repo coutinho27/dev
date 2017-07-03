702a0e8b341b18835f4aa021e14d1c2094eb618b
coutinho27 <luancoutinho18@gmail.com>
2017-07-03 00:21:06 -0300

    Correção Twilio

--- twilio_sms.py
+++ twilio_sms.py
@@ -1,8 +1,8 @@
 from twilio.rest import Client
 
 # Find these values at https://twilio.com/user/account
-account_sid = "AC2f88e71e257b6db99183ca43ae4b7000"
-auth_token = "48a57e95a4d23583039c7d6d7a15d4a0"
+account_sid = "AC2xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+auth_token = "564cdxzczxczxczxca5646543214"
 client = Client(account_sid, auth_token)
 
 #Teste com telefone do Paulo
